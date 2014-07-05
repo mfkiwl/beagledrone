3cfc639b0b5619100fe2dde0ee46114242a5d2fc
Mateusz Kaduk <mateusz.kaduk@gmail.com>
2014-07-05 19:04:22 +0000

    Use vector

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -13,25 +13,24 @@ i2c::~i2c() {
 	close(i2c_dev);
 }
 
-void i2c::writebus(uint8_t reg_addr, uint8_t *buff, size_t size) {
+std::vector<uint8_t> i2c::readbus(uint8_t reg_addr, size_t size) {
+	uint8_t write_buff[1]={0};
+	uint8_t read_buff[size]={0};
 	write_buff[0] = reg_addr;
-	memcpy(&write_buff[1], &buff, size);
-	ssize_t s;
-
-	if((s = write(i2c_dev, write_buff, size+1)) != (ssize_t)size+1){
-		throw std::runtime_error("error: wrote than one byte");
+	
+	if(write(i2c_dev, write_buff, 1) != 1){
+		throw std::runtime_error("error: wrote more than one byte");
 	}
+	std::cout << (unsigned int)read_buff[0] << std::endl; // DEBUG
+	if(read(i2c_dev, read_buff, size) != (ssize_t)size){
+		throw std::runtime_error("error: got less bytes than expected");
+	}
+	std::cout << (unsigned int)read_buff[0] << std::endl; // DEBUG
+	
+	std::vector<uint8_t> vect(size);
+	for(size_t i=0;i<size;i++) {
+		vect[i] = read_buff[i];
+	}
+	return(vect);
 }
 
-uint8_t* i2c::readbus(uint8_t reg_addr, size_t size) {
-	write_buff[0] = reg_addr;
-	ssize_t s, r;
-
-	if((s = write(i2c_dev, write_buff, 1)) != 1){
-		throw std::runtime_error("error: wrote than one byte");
-	}
-	if((r = read(i2c_dev, read_buff, size)) != (ssize_t)size){
-			throw std::runtime_error("error: read less bytes than expected");
-	}
-	return(read_buff);
-}
--- src/i2c.hpp
+++ src/i2c.hpp
@@ -13,24 +13,20 @@
 #include <linux/i2c-dev.h>
 /* memcpy */
 #include <cstring>
+/* uint_8 */
+#include <cstdint>
 
 #include <string>
 #include <iostream>
 #include <stdexcept>
-
-#define MAX_BUFFER_SIZE					64
+#include <vector>
 
 class i2c {
  private:
-  uint8_t i2c_dev;
-	uint8_t write_buff[MAX_BUFFER_SIZE];
-	uint8_t read_buff[MAX_BUFFER_SIZE];
-	
+  uint8_t i2c_dev;  
  public:
   i2c(std::string dev_path, uint8_t addr);
-  void writebus(uint8_t reg_addr, uint8_t *buff, size_t size);
-  uint8_t* readbus(uint8_t reg_addr, size_t size);
-  
+  std::vector<uint8_t> readbus(uint8_t reg_addr, size_t size);
   ~i2c();
 };
 #endif
