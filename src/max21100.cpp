8c60560a93d092e6cae8d3866a287ba5c2f6c908
Mateusz Kaduk <mateusz.kaduk@gmail.com>
2014-07-06 11:18:50 +0000

    Pass a pointer

--- src/max21100.cpp
+++ src/max21100.cpp
@@ -1,8 +1,8 @@
 #include "max21100.hpp"
 
 max21100::max21100() : i2cbus(new i2c("/dev/i2c-1", 0x58)) { // initialize i2c bus
-	i2cbus->writebus(0x00,0x7F);                               // power on
-  usleep(6000);                                              // wait for the device to start up
+	power_cfg pcfg(i2cbus);
+	pcfg.enable();
 }
 
 float max21100::readTemp() {
@@ -11,4 +11,15 @@ float max21100::readTemp() {
 
 max21100::~max21100() {
 	delete i2cbus;                                             // free bus
+}
+
+power_cfg::power_cfg(i2c *i2cbus_ptr) : i2cbus(i2cbus_ptr) {
+}
+
+power_cfg::~power_cfg() {
+}
+
+void power_cfg::enable() {
+	i2cbus->writebus(0x00,0x7F);                               // power on
+  usleep(6000);                                              // wait for the device to start up
 }
--- src/max21100.hpp
+++ src/max21100.hpp
@@ -2,14 +2,25 @@
 #define _MAX21100_H_
 
 #include "i2c.hpp"
+#include <memory>
 
 class max21100 {
  private:
   i2c *i2cbus;
  public:
+ 
   max21100();
   ~max21100();
   float readTemp();
+};
+
+class power_cfg {
+	private:
+		i2c *i2cbus;
+	public:
+	power_cfg(i2c *i2cbus);
+	~power_cfg();
+	void enable();
 };
 #endif
 
